3f35f90e35d38201feeb718a68afd06b59c51516
minutes1999 <minutes1999@outlook.com>
2017-08-19 19:26:01 +0800

    javascrit

---
+++ javascript_function.js
@@ -0,0 +1,8 @@
+/*
+* @Author: axii
+* @Date:   2017-08-19 12:55:12
+* @Last Modified by:   axii
+* @Last Modified time: 2017-08-19 14:20:51
+*/
+// document.write("hello world");
+// alert("u have press it");
