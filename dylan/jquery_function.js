5ac191b84add560c8536a3cd8e3ada7fcd3b1b75
minutes1999 <minutes1999@outlook.com>
2017-08-23 21:49:05 +0800

    hit skate

--- dylan/jquery_function.js
+++ dylan/jquery_function.js
@@ -2,12 +2,11 @@
 * @Author: axii
 * @Date:   2017-08-23 15:30:26
 * @Last Modified by:   axii
-* @Last Modified time: 2017-08-23 17:19:06
+* @Last Modified time: 2017-08-23 21:30:42
 */
-&(document).ready(function(){
-	$("")
-
-
-
+$(document).ready(function(){
+	$("#img_head").mouseover(function(){
+		$(this).hide();
+	});
 
 });
